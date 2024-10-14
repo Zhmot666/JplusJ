5e1c046c63b933ea6abfe2f4811b93ddde4a95e4
Zhmot666 <mailboxforspam@tut.by>
2024-10-14 07:23:35 +0300

    first

---
+++ main.py
@@ -0,0 +1,37 @@
+import json
+
+if __name__ == '__main__':
+    new_dict = dict()
+
+    with open('Kv4.json', encoding='cp1251') as kv4:
+        all_data2 = json.load(kv4)
+        all_employees2 = all_data2['data']
+        for key, value in all_data2.items():
+            if key != 'data':
+                new_dict[key] = value
+
+    with open('Kv3.json', encoding='cp1251') as kv3:
+        all_data1 = json.load(kv3)
+        all_employees1 = all_data1['data']
+
+    for fl in all_data2.items():
+        if key != 'data':
+            new_dict[key] = value
+    new_dict['data'] = list()
+
+    for i in all_employees2:
+        for j in all_employees1:
+            if i['ils'] == j['ils'] and i['kzl'] != '03':
+                temp_dict = dict()
+                for key, value in i.items():
+                    if key != 'r31' or key != 'r32':
+                        temp_dict[key] = value
+                    if key == 'r31' or key == 'r32':
+                        temp_dict[key] = j[key]+i[key]
+                new_dict['data'].append(temp_dict)
+                break
+        else:
+            new_dict['data'].append(i)
+
+    with open("out.txt", "w", encoding='cp1251') as outfile:
+        json.dump(new_dict, outfile)
